6907a1b796ae6022e00453c0d5812f6521ba70e6
sathyaeraj <sathyagovindaraj@gmail.com>
2019-10-13 07:03:24 -0400

    Different branch

---
+++ SampeAutomation/src/Branchautomate.java
@@ -0,0 +1,9 @@
+
+public class Branchautomate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Verifying different branch checkout");
+	}
+
+}
